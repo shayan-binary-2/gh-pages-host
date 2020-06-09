dea40fd2e6c788d4c883a078d852bacf47d2f698
shayan <shayan@firstsource.tech>
2020-06-09 10:14:29 +0430

    Updates

---
+++ precache-manifest.e9221adfc9e982433480b5aba812be1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb31f5d9c22447849608e8ca7f70863",
+    "url": "/gh-pages-host/index.html"
+  },
+  {
+    "revision": "4b6ac6eb725ef8cc05c1",
+    "url": "/gh-pages-host/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ddba61518ea4c9b8bfd",
+    "url": "/gh-pages-host/static/js/2.1764ae6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-host/static/js/2.1764ae6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b6ac6eb725ef8cc05c1",
+    "url": "/gh-pages-host/static/js/main.812fd6d8.chunk.js"
+  },
+  {
+    "revision": "e8120b28378758ec6e85",
+    "url": "/gh-pages-host/static/js/runtime-main.4ee20081.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-host/static/media/logo.5d5d9eef.svg"
+  }
+]);
